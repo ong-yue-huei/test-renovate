2ebd2d6a48e35caf344663f66344a832e0eecf97
ong.yue.huei <ongyuehuei@gmail.com>
2021-07-08 16:42:05 +0800

    install Nuke with Swift Package Manager implement nuke in EventTableViewCell

--- Training/Scenes/Main/Subviews/EventTableViewCell.swift
+++ Training/Scenes/Main/Subviews/EventTableViewCell.swift
@@ -1,11 +1,20 @@
 import UIKit
+import Nuke
 
 final class EventTableViewCell: UITableViewCell {
     func event(_ event: Event) {
         label.text = "\(event.type)"
         descriptionLabel.text = "\(event.actor.login)/\(event.repo.name)"
+        Nuke.loadImage(with: event.actor.avatarUrl, into: profileImage)
+//        Nuke.loadImage(with: event.actor.avatarUrl, options: options, into: profileImage)
     }
 
+//    let options = ImageLoadingOptions(
+//        placeholder: UIImage(named: "placeholder"),
+//        transition: .fadeIn(duration: 0.33),
+//        failureImage: UIImage(named: "oopsie")
+//    )
+    
     @IBOutlet var profileImage: UIImageView!
     @IBOutlet private var label: UILabel!
     @IBOutlet var descriptionLabel: UILabel!
