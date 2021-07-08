ae1b3e4830768a0de968c6b534dc94231f113cda
ong.yue.huei <ongyuehuei@gmail.com>
2021-07-08 16:26:19 +0800

    add label and imageView in EventTableViewCell.xib connect EventTableViewCell.xib with EventTableViewCell.swift

--- Training/Scenes/Main/Subviews/EventTableViewCell.swift
+++ Training/Scenes/Main/Subviews/EventTableViewCell.swift
@@ -2,8 +2,11 @@ import UIKit
 
 final class EventTableViewCell: UITableViewCell {
     func event(_ event: Event) {
-        label.text = "\(event.type) by \(event.actor.login) at \(event.repo.name)"
+        label.text = "\(event.type)"
+        descriptionLabel.text = "\(event.actor.login)/\(event.repo.name)"
     }
 
+    @IBOutlet var profileImage: UIImageView!
     @IBOutlet private var label: UILabel!
+    @IBOutlet var descriptionLabel: UILabel!
 }
