10cbb652a3fc8e5fa7c5733e1602c182e68f14e9
ong.yue.huei <ongyuehuei@gmail.com>
2021-07-08 18:24:38 +0800

    refactor profileImage in EventTableViewCell using didset{}

--- Training/Scenes/Main/Subviews/EventTableViewCell.swift
+++ Training/Scenes/Main/Subviews/EventTableViewCell.swift
@@ -3,20 +3,17 @@ import Nuke
 
 final class EventTableViewCell: UITableViewCell {
     func event(_ event: Event) {
-        label.text = "\(event.type)"
-        descriptionLabel.text = "\(event.actor.login)/\(event.repo.name)"
+        label.text = event.type
+        descriptionLabel.text = event.repo.name
         Nuke.loadImage(with: event.actor.avatarUrl, into: profileImage)
-        profileImage.layer.cornerRadius = profileImage.frame.size.width * 0.5
-//        Nuke.loadImage(with: event.actor.avatarUrl, options: options, into: profileImage)
     }
-
-//    let options = ImageLoadingOptions(
-//        placeholder: UIImage(named: "placeholder"),
-//        transition: .fadeIn(duration: 0.33),
-//        failureImage: UIImage(named: "oopsie")
-//    )
     
-    @IBOutlet var profileImage: UIImageView!
+    @IBOutlet var profileImage: UIImageView!{
+        didSet {
+            profileImage.clipsToBounds = true
+            profileImage.layer.cornerRadius = profileImage.frame.size.width * 0.5
+        }
+    }
     @IBOutlet private var label: UILabel!
     @IBOutlet var descriptionLabel: UILabel!
 }
